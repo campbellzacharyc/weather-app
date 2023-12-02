40acba5a29b90cc1597481db08157f3b23f7491a
zacharyccampbell <zacharydenzel23@gmail.com>
2023-12-01 19:04:28 -0600

    Update

--- weather.js
+++ weather.js
@@ -1,26 +1,22 @@
-// async function getWeather() {
-//     const response = await fetch('https://api.weatherapi.com/v1/current.json?key=74c0c557814f4abbbea20356232611&q=');
-//     const weatherData = await response.json();
-//     console.log(weatherData);
-// }
-
 async function getCity() {
     try {
         const city = document.querySelector('.city').value;
         const state = document.querySelector('.state').value;
         const country = document.querySelector('.country').value;
-        console.log("poop");
+
         console.log(city);
         console.log(state);
         console.log(country);
-        const response = await fetch('https://api.weatherapi.com/v1/current.json?key=74c0c557814f4abbbea20356232611&q=' + city + '_' + state + '_' + country, {mode: "cors"});
-        const weatherData = await response.json();
-        console.log("Fetching current weather data from API....", weatherData);
-        return weatherData;
+        
+        const response = await fetch('https://api.weatherapi.com/v1/forecast.json?key=74c0c557814f4abbbea20356232611&q=' + city + '_' + state + '_' + country + "&days=3", {mode: "cors"});
+        const currentData = await response.json();
+        console.log("Fetching current weather data from API....", currentData);
     } catch (err) {
-        return console.log(err);
+        console.log(err);
     }
 };
+
+
 
 const searchBtn = document.querySelector('button');
 searchBtn.addEventListener("click", getCity);
